66eaa57bd72856e76163cfd923afc1052e1f2c99
muradpm <abdulkadyr0v@yandex.ru>
2024-04-21 13:41:27 +0300

    add mobile sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { PanelLeftOpen } from "lucide-react";
+
+import Sidebar from "./sidebar";
+import SidebarLogo from "./sidebar-logo";
+import SidebarOrg from "./sidebar-org";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="md:hidden">
+        <Button variant="outline" size="icon">
+          <PanelLeftOpen className="h-4 w-4" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 pt-10 w-64">
+        <SidebarLogo />
+        <Sidebar />
+        <SidebarOrg />
+      </SheetContent>
+    </Sheet>
+  );
+};
