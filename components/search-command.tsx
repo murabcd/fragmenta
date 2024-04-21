d5c09059bd295f579cd07f144b80c01760c61888
muradpm <abdulkadyr0v@yandex.ru>
2024-04-21 13:42:26 +0300

    add search command

---
+++ components/search-command.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Input } from "./ui/input";
+
+import { Search } from "lucide-react";
+
+const SearchCommand = () => {
+  return (
+    <div className="relative hidden md:block mt-5 px-3">
+      <div className="relative mt-1 rounded-md shadow-sm">
+        <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+        <Input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          className="pl-10"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchCommand;
