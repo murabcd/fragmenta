9ff38f4d4237aab6a2b3fcfaa170a95887696a0f
muradpm <abdulkadyr0v@yandex.ru>
2024-04-21 13:42:36 +0300

    add org list

---
+++ components/sidebar-org.tsx
@@ -0,0 +1,10 @@
+const SidebarOrg = () => {
+  return (
+    <div className="px-3 text-sm font-medium">
+      Organizations
+      {/* <div className="mt-1 space-y-1">List of orgs</div> */}
+    </div>
+  );
+};
+
+export default SidebarOrg;
