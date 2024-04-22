529d0e8516f164f2926b49590121b5d537a5c047
muradpm <abdulkadyr0v@yandex.ru>
2024-04-23 00:06:09 +0300

    update sidebar logo

--- components/sidebar-logo.tsx
+++ components/sidebar-logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const SidebarLogo = () => {
   return (
-    <div className="flex flex-shrink-0 items-center px-1.5">
+    <div className="flex flex-shrink-0 items-center mt-2 px-1.5">
       <Image src="/logo.svg" alt="Sidebar logo" height={60} width={60} />
       <p className="text-xl font-semibold">fragmenta.ai</p>
     </div>
