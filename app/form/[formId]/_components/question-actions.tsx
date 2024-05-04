146d0c284e472cc88b6b48ddfd387dc877d4a963
muradpm <abdulkadyr0v@yandex.ru>
2024-05-04 16:02:35 +0300

    add question actions

---
+++ app/form/[formId]/_components/question-actions.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { toast } from "sonner";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+import { useApiMutation } from "@/hooks/use-api-mutation";
+
+import { api } from "@/convex/_generated/api";
+
+interface QuestionActionsProps {
+  children: React.ReactNode;
+  align?: "start" | "center" | "end";
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const QuestionActions = ({
+  children,
+  align,
+  side,
+  sideOffset,
+  id,
+}: QuestionActionsProps) => {
+  const { mutate } = useApiMutation(api.question.remove);
+  const { mutate: duplicate } = useApiMutation(api.question.duplicate);
+
+  const onDuplicate = () => {
+    duplicate({ id })
+      .then(() => toast.success("Question duplicated"))
+      .catch(() => toast.error("Failed to duplicate question"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => {
+        toast.success("Question deleted");
+      })
+      .catch(() => toast.error("Failed to delete question"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(event) => event.stopPropagation()}
+        align={align}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-[160px]"
+      >
+        <DropdownMenuItem onClick={onDuplicate}>Duplicate</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={onDelete}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
