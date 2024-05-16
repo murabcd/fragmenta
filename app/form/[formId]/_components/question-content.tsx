ebc3136bf7c7d77e6ba873347b8398190fee840b
muradpm <abdulkadyr0v@yandex.ru>
2024-05-16 22:28:54 +0300

    update question's content styles

--- app/form/[formId]/_components/question-content.tsx
+++ app/form/[formId]/_components/question-content.tsx
@@ -70,11 +70,11 @@ export const QuestionContent = ({ question }: QuestionContentProps) => {
   return (
     <Card className="flex flex-col items-center justify-center h-[600px] w-full max-w-4xl p-4 bg-secondary shadow-md rounded-lg space-y-4">
       <Input
-        className="bg-primary-foreground hover:bg-primary/10"
+        className="bg-primary-foreground text-lg hover:bg-primary/10"
         value={question.title}
       />
       <Input
-        className="bg-primary-foreground hover:bg-primary/10"
+        className="bg-primary-foreground text-muted-foreground hover:bg-primary/10"
         value={question.description}
       />
       {renderQuestionContent()}
