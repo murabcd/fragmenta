0e66eaed58d56a15f505342da26ca15fea519b25
muradpm <abdulkadyr0v@yandex.ru>
2024-06-18 22:53:17 +0300

    add icon to question items

--- components/question-item.tsx
+++ components/question-item.tsx
@@ -2,11 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 
-import { MoreHorizontal } from "lucide-react";
+import {
+  Text,
+  ListMinus,
+  CheckCheck,
+  ListTodo,
+  ListChecks,
+  BarChart,
+  MoreHorizontal,
+} from "lucide-react";
 
 import { QuestionActions } from "./question-actions";
 
-import { Question } from "@/types/canvas";
+import { Question, QuestionType } from "@/types/canvas";
 
 interface QuestionItemProps {
   question: Question;
@@ -14,13 +22,27 @@ interface QuestionItemProps {
   onClick: () => void;
 }
 
+const questionTypelcons = {
+  [QuestionType.Short]: Text,
+  [QuestionType.Long]: ListMinus,
+  [QuestionType.YesNo]: CheckCheck,
+  [QuestionType.Single]: ListTodo,
+  [QuestionType.Multiple]: ListChecks,
+  [QuestionType.Rating]: BarChart,
+};
+
 export const QuestionItem = ({ question, className, onClick }: QuestionItemProps) => {
+  const Icon = questionTypelcons[question.type];
+
   return (
     <ul
       onClick={onClick}
       className={`flex items-center justify-between m-2 p-3 cursor-pointer hover:text-foreground rounded-lg transition ${className}`}
     >
-      <span className="truncate max-w-[calc(100%-40px)]">{question.title}</span>
+      <div className="flex items-center truncate max-w-[80%]">
+        {Icon && <Icon className="h-4 w-4 mr-2 flex-shrink-0" />}
+        <span className="truncate">{question.title}</span>
+      </div>
       <QuestionActions id={question._id} title={question.title} align="end" side="right">
         <Button variant="ghost" size="icon">
           <MoreHorizontal className="h-4 w-4" />
