8389e67a0b5ea2061db4ee2c91c6ae6e32ed13f0
muradpm <abdulkadyr0v@yandex.ru>
2024-06-29 16:43:39 +0300

    add lightweight logo

--- components/sidebar-logo.tsx
+++ components/sidebar-logo.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const SidebarLogo = () => {
   return (
-    <div className="flex flex-shrink-0 items-center mt-2 px-1.5">
+    <div className="flex flex-shrink-0 items-center mt-2 px-1">
       <Image src="/logo.svg" alt="Sidebar logo" height={60} width={60} />
       <Link href="#">
-        <span className="text-lg font-semibold">fragmenta.ai</span>
+        <span className="text-lg font-semibold">fragmenta/ai</span>
       </Link>
     </div>
   );
